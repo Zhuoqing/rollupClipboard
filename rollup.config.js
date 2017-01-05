b72a900fb79f27201d8019ddae69611d3d461107
Zhuoqing Wu <zhwu@Microsoft.com>
2017-01-04 22:50:34 -0800

    add typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
+import typescript from 'rollup-plugin-typescript';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 export default {
   sourceMap: true,
   plugins: [
+    typescript({
+      typescript: require('typescript')
+    }),
     nodeResolve({
       jsnext: true,
       main: true,
